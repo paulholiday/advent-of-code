d873cd11c754cdc93df8c342034b658e3257b73f
paulholiday <paul.holiday@ralawise.com>
2021-01-27 15:37:13 +0000

    Day Five part 1

---
+++ src/main/java/org/paulholiday/DayFive.java
@@ -0,0 +1,92 @@
+package org.paulholiday;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DayFive {
+
+    int getHighestSeatId(List<String> boardingPasses) {
+        return boardingPasses.stream().collect(Collectors.summarizingInt(this::getSeatId)).getMax();
+    }
+
+    int getSeatId(String seatLocation) {
+        List<Character> column = new ArrayList<>();
+        List<Character> row = new ArrayList<>();
+
+        for (int i = 0; i < seatLocation.length(); i++) {
+            if (i < 7) {
+                row.add(seatLocation.charAt(i));
+            }
+            else {
+                column.add(seatLocation.charAt(i));
+            }
+        }
+
+        int columnNumber = getColumn(column);
+        int rowNumber = getRow(row);
+
+        return rowNumber * 8 + columnNumber;
+    }
+
+    int getColumn(List<Character> column) {
+        return reduceColumn(column, 0, 0, 7);
+    }
+
+    int reduceColumn(List<Character> column, int columnPointer, int lowerBound, int upperBound) {
+        if (column.get(columnPointer) == 'L') {
+            if (columnPointer == 2) {
+                return lowerBound;
+            }
+            return reduceColumn(column, columnPointer + 1, lowerBound, calculateUpperBound(lowerBound, upperBound));
+        }
+        else if (column.get(columnPointer) == 'R') {
+            if (columnPointer == 2) {
+                return upperBound;
+            }
+            return reduceColumn(column, columnPointer + 1, calculateLowerBound(lowerBound, upperBound), upperBound);
+        }
+
+        return -1;
+    }
+
+    int getRow(List<Character> row) {
+        return reduceRow(row, 0, 0, 127);
+    }
+
+    int reduceRow(List<Character> row, int rowPointer, int lowerBound, int upperBound) {
+        if (row.get(rowPointer) == 'F') {
+            if (rowPointer == 6) {
+                return lowerBound;
+            }
+            return reduceRow(row, rowPointer + 1, lowerBound, calculateUpperBound(lowerBound, upperBound));
+        }
+        else if (row.get(rowPointer) == 'B') {
+            if (rowPointer == 6) {
+                return upperBound;
+            }
+            return reduceRow(row, rowPointer + 1, calculateLowerBound(lowerBound, upperBound), upperBound);
+        }
+
+        return -1;
+    }
+
+    int calculateLowerBound(int lowerBound, int upperBound) {
+        if (lowerBound == 0) {
+            return Math.floorDiv(upperBound, 2) + 1;
+        }
+        int boundDifference = upperBound - lowerBound;
+
+        return (lowerBound + Math.floorDiv(boundDifference, 2)) + 1;
+    }
+
+    int calculateUpperBound(int lowerBound, int upperBound) {
+        if (lowerBound == 0) {
+            return Math.floorDiv(upperBound + lowerBound, 2);
+        }
+
+        int boundDifference = upperBound - lowerBound;
+
+        return (upperBound - Math.floorDiv(boundDifference, 2)) - 1;
+    }
+}
---
+++ src/test/java/org/paulholiday/DayFiveTest.java
@@ -0,0 +1,55 @@
+package org.paulholiday;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DayFiveTest {
+
+    private List<String> boardingPasses;
+    private DayFive dayFive;
+
+    @BeforeEach
+    void setup() {
+
+        Utils utils = new Utils();
+
+        boardingPasses = utils.getFileContentsByLine("src/main/resources/DayFiveInput.txt");
+        dayFive = new DayFive();
+    }
+
+    @Test
+    void getColumn() {
+
+        assertEquals(7, dayFive.getColumn(Arrays.asList('R', 'R', 'R')));
+        assertEquals(0, dayFive.getColumn(Arrays.asList('L', 'L', 'L')));
+        assertEquals(4, dayFive.getColumn(Arrays.asList('R', 'L', 'L')));
+
+    }
+
+    @Test
+    void getRow() {
+
+        assertEquals(70, dayFive.getRow(Arrays.asList('B', 'F', 'F', 'F', 'B', 'B', 'F')));
+        assertEquals(14, dayFive.getRow(Arrays.asList('F', 'F', 'F', 'B', 'B', 'B', 'F')));
+        assertEquals(102, dayFive.getRow(Arrays.asList('B', 'B', 'F', 'F', 'B', 'B', 'F')));
+    }
+
+    @Test
+    void getSeatId() {
+
+        assertEquals(567, dayFive.getSeatId("BFFFBBFRRR"));
+        assertEquals(119, dayFive.getSeatId("FFFBBBFRRR"));
+        assertEquals(820, dayFive.getSeatId("BBFFBBFRLL"));
+    }
+
+    @Test
+    void highestSeatId() {
+
+        System.out.println(dayFive.getHighestSeatId(boardingPasses));
+    }
+}
