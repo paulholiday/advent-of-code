2a77153e9b00337fb39461405eb016b884ab09e2
paulholiday <paul.holiday@ralawise.com>
2021-01-28 17:26:49 +0000

    Day 6 part 2

--- src/main/java/org/paulholiday/DaySix.java
+++ src/main/java/org/paulholiday/DaySix.java
@@ -1,14 +1,22 @@
 package org.paulholiday;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class DaySix {
 
     int getNumberOfUniqueQuestionsAnswerYes(String answers) {
+        return getSetOfUniqueAnswers(answers).size();
+    }
 
+    int getNumberOfUniqueQuestionsAnsweredYesFromInput(List<String> answersList) {
+        return answersList.stream().mapToInt(this::getNumberOfUniqueQuestionsAnswerYes).sum();
+    }
+
+    Set<Character> getSetOfUniqueAnswers(String answers) {
         Set<Character> uniqueAnswers = new HashSet<>();
 
         for (int i = 0; i < answers.length(); i++) {
@@ -19,10 +27,35 @@ public class DaySix {
             }
         }
 
-        return uniqueAnswers.size();
+        return uniqueAnswers;
     }
 
-    int getNumberOfUniqueQuestionsAnsweredYesFromInput(List<String> answersList) {
-        return answersList.stream().mapToInt(this::getNumberOfUniqueQuestionsAnswerYes).sum();
+    int getCombinedNumberOfAnswersWhereEveryoneAnsweredYes(List<String> answersList) {
+        return answersList.stream().mapToInt(this::getNumberOfAnswersWhereEveryoneAnsweredYes).sum();
+    }
+
+    int getNumberOfAnswersWhereEveryoneAnsweredYes(String answers) {
+
+        Set<Character> uniqueAnswers = getSetOfUniqueAnswers(answers);
+        int numberOfMultipleAnswers = 0;
+
+        String[] answersArray = answers.split(" ");
+
+        if (answersArray.length > 1) {
+
+            for (char uniqueAnswer : uniqueAnswers) {
+                int count = StringUtils.countMatches(answers, uniqueAnswer);
+
+                if (count > 1 && count == answersArray.length) {
+                    numberOfMultipleAnswers++;
+                }
+            }
+
+        }
+        else {
+            numberOfMultipleAnswers += answersArray[0].length();
+        }
+
+        return numberOfMultipleAnswers;
     }
 }
--- src/test/java/org/paulholiday/DaySixTest.java
+++ src/test/java/org/paulholiday/DaySixTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -41,5 +43,37 @@ class DaySixTest {
         assertEquals(34, daySix.getNumberOfUniqueQuestionsAnsweredYesFromInput(answers));
 
         System.out.println(daySix.getNumberOfUniqueQuestionsAnsweredYesFromInput(answersList));
+    }
+
+    @Test
+    void getSetOfUniqueAnswers() {
+
+        Set<Character> uniqueAnswers = new HashSet<>();
+        uniqueAnswers.add('a');
+        uniqueAnswers.add('b');
+        uniqueAnswers.add('c');
+
+        assertEquals(uniqueAnswers, daySix.getSetOfUniqueAnswers("abc"));
+    }
+
+    @Test
+    void getNumberOfMultipleAnswers() {
+
+        assertEquals(3, daySix.getNumberOfAnswersWhereEveryoneAnsweredYes("abc "));
+        assertEquals(0, daySix.getNumberOfAnswersWhereEveryoneAnsweredYes("a b c "));
+        assertEquals(1, daySix.getNumberOfAnswersWhereEveryoneAnsweredYes("ab ac "));
+        assertEquals(1, daySix.getNumberOfAnswersWhereEveryoneAnsweredYes("a a a a "));
+        assertEquals(1, daySix.getNumberOfAnswersWhereEveryoneAnsweredYes("b "));
+        assertEquals(0, daySix.getNumberOfAnswersWhereEveryoneAnsweredYes("zmd alo ekvrtpyuqi hwmc szgah "));
+    }
+
+    @Test
+    void getCombinedNumberOfMultipleAnswers() {
+
+        List<String> answers = Arrays.asList("abc ", "a b c ", "ab ac ", "a a a a ", "b ");
+
+        assertEquals(6, daySix.getCombinedNumberOfAnswersWhereEveryoneAnsweredYes(answers));
+
+        System.out.println(daySix.getCombinedNumberOfAnswersWhereEveryoneAnsweredYes(answersList));
     }
 }
